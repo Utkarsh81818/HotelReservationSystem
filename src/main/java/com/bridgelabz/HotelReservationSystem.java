203406198e82dcc8bfd5d6589827d23e79a82c3d
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-10-12 08:45:00 +0530

    Added: Ability to add hotels

--- src/main/java/com/bridgelabz/HotelReservationSystem.java
+++ src/main/java/com/bridgelabz/HotelReservationSystem.java
@@ -1,7 +1,20 @@
 package com.bridgelabz;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Purpose  - To build a Hotel Reservation System
+ * Author   - Utkarsh Mishra
+ * @version - 16.0
+ * @since   - 2021-10-12
+ */
 
 public class HotelReservationSystem {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Hotel Reservation System Program");
+     List<Hotel> hotels = new ArrayList<>();
+     /**
+     * This method is used to add hotel to the Hotel List
+     */
+    public boolean addHotel(Hotel hotel) {
+        return hotels.add(hotel);
     }
 }
+
